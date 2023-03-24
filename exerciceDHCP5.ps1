c727b3c136b63d3ee85880c1b5c52fd65faaffab
Kilian Gazeau <kilian.gazeau@local.dortmund.cub.sioplc.fr>
2023-03-24 16:58:00 +0100

    exercice

--- exerciceDHCP5.ps1
+++ exerciceDHCP5.ps1
@@ -4,9 +4,10 @@
 #DATE:28/02/2023
 #
 #Version 1.0
-#Comments: Script qui permet de configurer automatiquement une plage d'adresse, pour le DHCP, 
+#Comments: Script qui permet de configurer automatiquement une plage d'adresse, pour le DHCP,
 #avec le nom de domaine et l'adresse IP du serveur de domaine tout cela en boîtes de dialogue
-#et laisse la possibilité de créer plusieurs plage rapidement
+#Laisse la possibilité de créer plusieurs plage rapidement
+#Possibilité de réserver une adresse IP
 #
 #=====================================================#
 
@@ -31,6 +32,11 @@ $1hote = [Microsoft.VisualBasic.Interaction]::InputBox("Quelle est la première 
 $der_hote = [Microsoft.VisualBasic.Interaction]::InputBox("Quelle est la dernière adresse à distribuer ?")
 $passerelle = [Microsoft.VisualBasic.Interaction]::InputBox("Quelle est l'adresse de la passerelle ?")
 
+# Varibale pour la demande de réservation de plage d'adresse
+$ad_Ip = [Microsoft.VisualBasic.Interaction]::InputBox("Quelle addresse voulez-vous réserver ? ")
+$ad_mac = [Microsoft.VisualBasic.Interaction]::InputBox("Quelle addresse MAC voulez-vous réserver ? ")
+$desc = [Microsoft.VisualBasic.Interaction]::InputBox("Entrez une description pour votre adresse réservé ? ")
+
 # Affiche toutes les variables afin de faire un récapitulatif
 
 [Microsoft.VisualBasic.Interaction]::MsgBox("Nom de l'étendue : $name `nAdresse de réseau : $ad_Reseau `nMasque: $mask `nPremière adresse : $1hote `nDernière adresse : $der_hote `nPasserelle : $passerelle")
@@ -46,7 +52,8 @@ $Confirm = [Microsoft.VisualBasic.Interaction]::InputBox("Cette configuration vo
 if(($Confirm -eq "O") -or ($filtre -eq "o"))
 {
     Add-DhcpServerv4Scope -Name "$name" -StartRange $1hote -EndRange $der_hote -SubnetMask $mask -Description "nul"
-    Set-DhcpServerv4OptionValue -DNSServer 9.9.9.9 -WinsServer 192.168.4.1 -DNSDomain local.dortmund.cub.sioplc.fr -Router $passerelle 
+    Set-DhcpServerv4OptionValue -DNSServer 9.9.9.9 -WinsServer 192.168.4.1 -DNSDomain local.dortmund.cub.sioplc.fr -Router $passerelle
+    Add-DhcpServerv4Reservation -ScopeId 192.168.4.0 -IPAddress $ad_Ip -ClientId $ad_mac -Description $desc
 }
 Else
 {
